dab6ab384644cd2f60434c1bc1b05d0b78373ea8
anutashah98 <79895916+anutashah98@users.noreply.github.com>
2023-03-21 16:50:48 +0300

    MainController

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameStateManager : MonoBehaviour
+    {
+        public static GameStateManager Instance;
+        
+        private bool _isDead = false;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(Instance.gameObject);
+                return;
+            }
+            
+            Instance = this;
+            
+        }
+
+        public void Die()
+        {
+            Debug.Log("Player Died");
+            _isDead = true;
+        }
+    }
+}
