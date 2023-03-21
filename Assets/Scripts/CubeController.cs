a0990af186a083a0a59596ad92b53f90c7978295
anutashah98 <79895916+anutashah98@users.noreply.github.com>
2023-03-21 15:14:44 +0300

    init2

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -6,11 +6,23 @@ using UnityEngine.EventSystems;
 
 public class CubeController : MonoBehaviour
 {
+    [SerializeField] private float _rollSpeed = 5f;
     private Vector3 _pivotPoint;
     private Vector3 _axis;
+    private bool _isMoving;
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
 
     void Update()
     {
+        if (_isMoving) return;
+        {
+            
+        }
         if (Input.GetKey(KeyCode.A))
         {
             Move(Vector3.left);
@@ -31,19 +43,44 @@ public class CubeController : MonoBehaviour
 
     private void Move(Vector3 direction)
     {
-        _pivotPoint = (direction / 2f) + (Vector3.down / 2f) + transform.position;
-        _axis = Vector3.Cross(Vector3.up, direction); 
+        var verticalComponent = Vector3.down;
+        var hasWall = HasWallInDirection(direction);
         
-        var pos = transform.position;
+        if (hasWall)
+        {
+            verticalComponent = Vector3.up;
+        }
+        
+        _pivotPoint = (direction / 2f) + (verticalComponent / 2f) + transform.position;
+        _axis = Vector3.Cross(Vector3.up, direction);
 
-        pos = pos + direction * Time.deltaTime;
+        StartCoroutine(Roll(_pivotPoint, _axis));
+    }
 
-        transform.position = pos;
+    private bool HasWallInDirection(Vector3 direction)
+    {
+        return Physics.Raycast(transform.position, direction, 0.55f);
+    }
+
+    private IEnumerator Roll(Vector3 pivot, Vector3 axis)
+    {
+        _isMoving = true;
+        _rigidbody.isKinematic = true;
+
+        for (int i = 0; i < 90 / _rollSpeed; i++)
+        {
+            transform.RotateAround(pivot, axis, _rollSpeed);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        _rigidbody.isKinematic = false;
+        _isMoving = false;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(_pivotPoint,0.2f);
+        Gizmos.DrawRay(_pivotPoint,_axis);
     }
 }
